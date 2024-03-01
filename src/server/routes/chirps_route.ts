ca5c21064c3064b08acf1aceb9d1a6221a02de1a
Blake-sizemore <magnify1more@gmail.com>
2024-02-29 23:04:49 -0500

    basic working CRUD, need to add the update feature

--- src/server/routes/chirps_route.ts
+++ src/server/routes/chirps_route.ts
@@ -3,13 +3,50 @@ import db from '../db';
 
 const router = Router();
 
+// Working
+router.get('/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const chirps = await db.chirps_db.getOne(id);
+        res.json(chirps);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'internal server error test', error });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        await db.chirps_db.deleteOne(id);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'internal server error on delete', error });
+    }
+});
+
 router.get('/', async (req, res) => {
     try {
         const chirps = await db.chirps_db.getAll();
         res.json(chirps);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'internal server error', error });
+        res.status(500).json({ message: 'internal server error getAll', error });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const newAdd = req.body;
+        const fixedUser = newAdd.user_id = 1;
+        // Not sure if the above const is a workable solution but it got processed correctly in my post
+        const result = await db.chirps_db.addChirp(fixedUser,newAdd.message,newAdd.city);
+        res.json({message: 'Chirp created and sent', id: result.insertId});
+    } catch (error) {
+        const newChirp = req.body
+        console.log(error, newChirp);
+        res.status(500).json({ message: 'internal server error post', error });
     }
 });
 
